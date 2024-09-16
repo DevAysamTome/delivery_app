891fba0308e3858752f273cdcf8da4563385f1b9
Aysam Tohme <130704828+DevAysamTome@users.noreply.github.com>
2024-09-16 16:57:19 +0300

    first commit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,20 +1,37 @@
-import Flutter
 import UIKit
-import Firebase  // استيراد Firebase
+import Flutter
+import GoogleMaps
+import Firebase
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  
+@objc class AppDelegate: FlutterAppDelegate, CLLocationManagerDelegate {
+  var locationManager: CLLocationManager?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
+    FirebaseApp.configure()  
+    GMSServices.provideAPIKey("AIzaSyBzdajHgG7xEXtoglNS42Jbh8NdMUj2DXk")
     GeneratedPluginRegistrant.register(with: self)
     
-    // تهيئة Firebase
-    // لقد تمت إزالة دالة طلب إذن التتبع
-    
+    locationManager = CLLocationManager()
+    locationManager?.delegate = self
+    locationManager?.requestWhenInUseAuthorization()
+    locationManager?.startUpdatingLocation()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+  
+  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    switch status {
+    case .authorizedWhenInUse, .authorizedAlways:
+      locationManager?.startUpdatingLocation()
+    case .denied, .restricted:
+      
+      break
+    default:
+      break
+    }
+  }
+  
 }
