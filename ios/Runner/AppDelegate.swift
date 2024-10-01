36f0f4ba0c1b1ee30dcc5e14fec7c7ee1854645d
Aysam Tohme <130704828+DevAysamTome@users.noreply.github.com>
2024-10-01 16:23:14 +0300

    first commit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,7 +5,7 @@ import Firebase
 import UserNotifications
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, CLLocationManagerDelegate, UNUserNotificationCenterDelegate {
+@objc class AppDelegate: FlutterAppDelegate, CLLocationManagerDelegate {
 
   var locationManager: CLLocationManager?
 
@@ -74,14 +74,14 @@ import UserNotifications
   }
 
   // UNUserNotificationCenterDelegate method for foreground notifications
-  override func userNotificationCenter(_ center: UNUserNotificationCenter,
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
                               willPresent notification: UNNotification,
                               withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
     completionHandler([.alert, .badge, .sound])
   }
 
   // UNUserNotificationCenterDelegate method for handling user interaction with notifications
-  override func userNotificationCenter(_ center: UNUserNotificationCenter,
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
                               didReceive response: UNNotificationResponse,
                               withCompletionHandler completionHandler: @escaping () -> Void) {
     // Handle the notification interaction here
