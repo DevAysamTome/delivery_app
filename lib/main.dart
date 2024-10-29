1f9284c49933286531e1e544126e6cf5e4601e1f
Aysam Tohme <130704828+DevAysamTome@users.noreply.github.com>
2024-10-29 02:19:42 +0200

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,33 @@
 import 'dart:io';
 
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'package:delivery_app/res/components/notification_handler.dart';
 import 'package:delivery_app/views/home/home_screen.dart';
 import 'package:delivery_app/views/login_views/login_view.dart';
 import 'package:delivery_app/views/profile/profile_screen.dart';
 import 'package:delivery_app/views/weclome_screen/welcome_screen.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyBIY3TUh1CMt72v-LbLYY7RACHISkY_MlA",
+            appId: "1:572219831254:web:3c0d5c6d4b00ae7a45ea77",
+            messagingSenderId: "572219831254",
+            storageBucket: "sarie-46b77.appspot.com",
+            projectId: "sarie-46b77"));
+  } else {
+    await Firebase.initializeApp();
+  }
   // Notification handler instance
   NotificationHandler notificationHandler = NotificationHandler();
 
-  if (Platform.isIOS) {
+  if (!kIsWeb && Platform.isIOS) {
     String? apnsToken = await FirebaseMessaging.instance.getAPNSToken();
 
     if (apnsToken != null) {
@@ -34,7 +46,16 @@ void main() async {
   } else {
     print("This platform does not support APNs.");
   }
-
+  Future<void> requestTrackingPermission() async {
+  final status = await AppTrackingTransparency.trackingAuthorizationStatus;
+  if (status == TrackingStatus.notDetermined) {
+    await AppTrackingTransparency.requestTrackingAuthorization();
+  }
+  // Only proceed with tracking if the user grants permission
+  if (status == TrackingStatus.authorized) {
+    // Start tracking as per your app logic
+  }
+}
   runApp(MyApp(notificationHandler: notificationHandler)); // Pass the handler
 }
 
@@ -50,13 +71,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Delivery App',
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Elmassry'),
-      initialRoute: '/',
+      initialRoute: '/home',
       routes: {
         '/': (context) => const WelcomeScreen(),
         '/login': (context) => LoginScreen(),
         '/home': (context) => HomeScreen(),
         '/profile': (context) => SettingsScreen(),
       },
+      debugShowCheckedModeBanner: false,
     );
   }
 }
